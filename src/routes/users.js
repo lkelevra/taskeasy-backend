7252bc2bd2ca68fc06f1eb2d803d3073f417b267
Luis Gonzalez <luisalfredog@outlook.com>
2019-09-21 13:30:46 -0600

    User Routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,5 +4,13 @@ import UserController from '../controllers/UserController';
 const router = express.Router();
 
 router.get('/', UserController.listUsers);
+router.get('/disponibility', UserController.getScheduleAvailableUsers);
+router.get('/:id/disponibility', UserController.getDisponibilityByUserId);
+router.get('/:id/meetings', UserController.getMeetingsByUserId);
+router.get('/:id', UserController.findById);
+
+
+
+
 
 export default router;
