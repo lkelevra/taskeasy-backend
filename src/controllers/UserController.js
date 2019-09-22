01ac94b3b6d0d20d24da12cd9dc7c090fcdb86da
Luis Gonzalez <luisalfredog@outlook.com>
2019-09-22 13:05:13 -0600

    Format functions UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,36 +1,35 @@
-import UserRepository from '../models/repositories/UserRepository'
-import ScheduleRepository from '../models/repositories/ScheduleRepository'
+import UserRepository from "../models/repositories/UserRepository"
+import ScheduleRepository from "../models/repositories/ScheduleRepository"
 function getMeetingsByUserId(req, res) {
   let id = req.params.id
   UserRepository.getMeetingsByUserId(id)
-    .then((data) => res.json(data))
-    .catch((errors) => res.status(404).json({ errors }))
-
+    .then(data => res.json(data))
+    .catch(errors => res.status(404).json({ errors }))
 }
 
 function getScheduleAvailableUsers(req, res) {
-  ScheduleRepository.getSchedule().then(fullSchedule => {
-
-    let fullScheduleParsed = parseSchedule(fullSchedule)
-    UserRepository.list()
-      .then((users) => {
-
-        users.forEach((user, index) => {
-
-          let _user = { id: user.id, name: user.nombre }
-          let userSchedule = user.meetings
-          fullSchedule.forEach((itemFullSchedule, indexFullSchedule) => {
-            let existSchedule = existInScheduleOfUser(userSchedule, itemFullSchedule)
-            if (!existSchedule)
-              fullScheduleParsed[indexFullSchedule].users.push(_user)
+  ScheduleRepository.getSchedule()
+    .then(fullSchedule => {
+      let fullScheduleParsed = parseSchedule(fullSchedule)
+      UserRepository.list()
+        .then(users => {
+          users.forEach((user, index) => {
+            let _user = { id: user.id, name: user.nombre }
+            let userSchedule = user.meetings
+            fullSchedule.forEach((itemFullSchedule, indexFullSchedule) => {
+              let existSchedule = existInScheduleOfUser(
+                userSchedule,
+                itemFullSchedule
+              )
+              if (!existSchedule)
+                fullScheduleParsed[indexFullSchedule].users.push(_user)
+            })
           })
+          res.json(fullScheduleParsed)
         })
-        res.json(fullScheduleParsed)
-      })
-      .catch((errors) => res.status(404).json({ errors }))
-
-  }).catch(errors => res.status(404).json({ errors }))
-
+        .catch(errors => res.status(404).json({ errors }))
+    })
+    .catch(errors => res.status(404).json({ errors }))
 }
 function parseSchedule(fullSchedule) {
   return fullSchedule.map(item => ({ ...item, users: [] }))
@@ -44,21 +43,23 @@ function existInScheduleOfUser(userSchedule, schedule) {
     }
   })
   return existSchedule
-
 }
 function getDisponibilityByUserId(req, res) {
   let id = req.params.id
   UserRepository.getMeetingsByUserId(id)
-    .then((data) => {
+    .then(data => {
       let userScheduleParsed = parseUserSchedule(data)
-      ScheduleRepository.getSchedule().then(schedule => {
-        let userAvailableSchedule = diferenceSchedule(schedule, userScheduleParsed)
-        res.json(userAvailableSchedule)
-      }).catch(errors => res.status(404).json({ errors }))
-
+      ScheduleRepository.getSchedule()
+        .then(schedule => {
+          let userAvailableSchedule = diferenceSchedule(
+            schedule,
+            userScheduleParsed
+          )
+          res.json(userAvailableSchedule)
+        })
+        .catch(errors => res.status(404).json({ errors }))
     })
-    .catch((errors) => res.status(404).json({ errors }))
-
+    .catch(errors => res.status(404).json({ errors }))
 }
 
 function parseUserSchedule(data) {
@@ -68,23 +69,29 @@ function parseUserSchedule(data) {
 }
 
 function diferenceSchedule(schedule, userSchedule) {
-  return schedule.map(itemSchedule => itemSchedule.hour).filter(item => {
-    return userSchedule.indexOf(item) < 0
-  });
+  return schedule
+    .map(itemSchedule => itemSchedule.hour)
+    .filter(item => {
+      return userSchedule.indexOf(item) < 0
+    })
 }
 function findById(req, res) {
   let id = req.params.id
 
   UserRepository.findById(id)
-    .then((data) => res.json(data))
-    .catch((errors) => res.json({ errors }))
+    .then(data => res.json(data))
+    .catch(errors => res.json({ errors }))
 }
 function listUsers(req, res) {
-
   UserRepository.list()
-    .then((data) => res.json(data))
-    .catch((errors) => res.json({ errors }))
-
+    .then(data => res.json(data))
+    .catch(errors => res.json({ errors }))
 }
 
-export default { listUsers, findById, getMeetingsByUserId, getDisponibilityByUserId, getScheduleAvailableUsers };
+export default {
+  listUsers,
+  findById,
+  getMeetingsByUserId,
+  getDisponibilityByUserId,
+  getScheduleAvailableUsers
+}
